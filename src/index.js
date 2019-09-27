fafd4965dc363f5c3dafa8eec0672c7d1f8cd4ce
Th5tch3r <amber.phan27@gmail.com>
2019-09-27 14:32:08 -0400

    mvp

--- src/index.js
+++ src/index.js
@@ -5,5 +5,15 @@ import App from './App';
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import { reducer } from './store/reducer';
+
+const store = createStore(reducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+, rootElement);
